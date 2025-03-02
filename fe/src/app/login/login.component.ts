c16468375ccb3fa45e730e3d22ed76baf4620955
Fidzlokal <mhafidznr@gmail.com>
2025-03-02 12:58:05 +0700

    tampilan login

--- fe/src/app/login/login.component.ts
+++ fe/src/app/login/login.component.ts
@@ -1,25 +1,29 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [FormsModule, CommonModule]
 })
 export class LoginComponent {
   u: string = '';
   p: string = '';
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, public router: Router) {} // Router diubah ke public
 
   onLogin() {
     const body = new URLSearchParams();
     body.set('u', this.u);
     body.set('p', this.p);
-    
+
     const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
-    
+
     this.http.post<any>('http://localhost/mahiahijab/api/auth/login.php', body.toString(), { headers })
       .subscribe(
         response => {

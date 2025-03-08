9d8471da3bc4dfe8d599994460feee4d87df629c
Fidzlokal <mhafidznr@gmail.com>
2025-03-08 22:59:30 +0700

    Sedikit tampilan

--- api/admin/auth/check.php
+++ api/admin/auth/check.php
@@ -2,7 +2,7 @@
 session_start();
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 
